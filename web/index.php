d2a8d79f6272a2fe84d1f8298986bd1224cec3ce
trevormw20 <trevormw20@gmail.com>
2021-01-16 21:51:51 -0800

    style update

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WDD341 Introduction</title>
     <meta name="description" content="Trevor Williams Introduction for WDD 341">
-    <link rel="stylesheet" type="text/css" href="css/style.css?version=1">
+    <link rel="stylesheet" type="text/css" href="css/style.css?version=2">
 </head>
 
 <body>
