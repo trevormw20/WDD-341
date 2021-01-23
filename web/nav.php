3314b4e940a01456cca6480ce5ff6b58249aa943
trevormw20 <trevormw20@gmail.com>
2021-01-22 18:58:14 -0800

    assignments link fixed

--- web/nav.php
+++ web/nav.php
@@ -1,5 +1,5 @@
     <?php
     $navLinks = "<ul>";
     $navLinks .= "<li><a href='#'>Home</a></li>";
-    $navLinks .= "<li><a href='#'>Asssignments</a></li>";
+    $navLinks .= "<li><a href='assignments.html'>Asssignments</a></li>";
      echo $navLinks; ?>
