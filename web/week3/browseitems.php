9e95286cc14b1aaa358fb24d5edaf0f5b89da0f5
trevormw20 <trevormw20@gmail.com>
2021-01-30 20:35:30 -0800

    added database structure

--- web/week3/browseitems.php
+++ web/week3/browseitems.php
@@ -129,12 +129,3 @@ $_SESSION["test"] = "working";?>
 </body>
 
 </html>
-
-
-
-
-On the browse items page, the user sees a list of items they can add to their cart and
-purchase. Again, the kind of items and the formatting of this page is up to you.
-
-You should provide a button or link to add an item to the cart. Doing so should store
-that item in some way to the session, and then keep the user on the browse page.
--- web/week3/viewcart.php
+++ web/week3/viewcart.php
@@ -32,9 +32,6 @@
 
 </html>
 
-Your browse page should contain a link to view the cart. On the view cart page, 
+ On the view cart page, 
 the user can see all the items that are in their cart. Provide a way to remove 
 individual items from the cart.
-
-The view cart page should have a link to return to the browse page for more 
-shopping and a link to continue to the checkout page.
