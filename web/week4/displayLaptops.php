8369cc8799c8604aa0ed9d21362a5a05ea43a23f
trevormw20 <trevormw20@gmail.com>
2021-02-05 23:42:48 -0800

    week 4 fix

--- web/week4/displayLaptops.php
+++ web/week4/displayLaptops.php
@@ -1,42 +1,10 @@
-<?php 
 
-function get_db() {
-	$db = NULL;
-
-	try {
-		$dbUrl = getenv('DATABASE_URL');
-
-		if (!isset($dbUrl) || empty($dbUrl)) {
-			$dbUrl = "postgres://ta_user:ta_pass@localhost:5432/scripture_ta";
-		}
-
-		$dbopts = parse_url($dbUrl);
-
-		$dbHost = $dbopts["host"];
-		$dbPort = $dbopts["port"];
-		$dbUser = $dbopts["user"];
-		$dbPassword = $dbopts["pass"];
-		$dbName = ltrim($dbopts["path"],'/');
-
-		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-        $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	}
-	catch (PDOException $ex) {
-		echo "Error connecting to DB. Details: $ex";
-		die();
-	}
-
-	return $db;
-}
-
-?>
 
 
 <?php 
 //laptops controller
 //require_once $_SERVER['DOCUMENT_ROOT'] . '/WDD-341/web/week4/laptop-model.php';
-//require_once $_SERVER['DOCUMENT_ROOT'] . '/WDD-341/web/week4/dbConnect.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/WDD-341/web/week4/dbConnect.php';
 
 $db = get_db();
 
@@ -52,19 +20,10 @@ $laptops = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <?php //if (isset($laptopDisplay)) {
 		//echo $laptopDisplay;}
 
-		echo "<p>Test 1</p>";
-		echo $laptops[0][0]['laptopModel'];
-		echo "<p>Test 2</p>";
+
 		echo $laptops[0]['laptopmodel'];
 
-		echo '<br><br><pre>'; print_r($laptops[0]); echo '</pre>';
-		
 
-		echo '<br><br><pre>'; print_r($laptops); echo '</pre>';
-		echo "<p>Test 3</p>";
-		echo $laptops[0]["laptopmodel"];
-		echo "<p>Test 4</p>";
-		echo $laptops[0][0]["laptopmodel"];
 	 ?>
 
 </main>
