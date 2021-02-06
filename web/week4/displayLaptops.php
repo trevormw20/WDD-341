e42208c4b7db0cf016759d1398439990e36f7c25
trevormw20 <trevormw20@gmail.com>
2021-02-05 23:12:04 -0800

    week 4 fix

--- web/week4/displayLaptops.php
+++ web/week4/displayLaptops.php
@@ -1,17 +1,15 @@
+
+
 <?php 
+/*
 $query = 'SELECT * FROM laptops';
 $stmt = $db($query);
 $stmt->execute();
 $laptops = $stmt->fetchAll(PDO::FETCH_ASSOC);
+*/
 ?>
 
-<title><?php echo "$vehicle[invMake] $vehicle[invModel]"; ?> vehicles | PHP Motors, Inc.</title>
-
 <main>
-<?php
-if (isset($message)) {
-	echo $message;
-} ?>
 
 <?php //if (isset($laptopDisplay)) {
 		//echo $laptopDisplay;}
@@ -20,7 +18,3 @@ echo $laptops;
 	 ?>
 
 </main>
-
-
-
-<?php require $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer.php'; ?>
