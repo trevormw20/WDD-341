b6dbbe84527c62a6dbc6ba8df192e81ade577594
trevormw20 <trevormw20@gmail.com>
2021-02-05 23:21:07 -0800

    week 4 fix

--- web/week4/displayLaptops.php
+++ web/week4/displayLaptops.php
@@ -40,7 +40,7 @@ function get_db() {
 
 $db = get_db();
 
-//$query = 'SELECT * FROM laptops';
+$query = 'SELECT * FROM laptops';
 //$stmt = $db($query);
 //$stmt->execute();
 //$laptops = $stmt->fetchAll(PDO::FETCH_ASSOC);
