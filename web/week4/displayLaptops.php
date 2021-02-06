61ce1064014748f916fde2f894bffc5833d2b25f
trevormw20 <trevormw20@gmail.com>
2021-02-05 23:14:56 -0800

    week 4 fix

--- web/week4/displayLaptops.php
+++ web/week4/displayLaptops.php
@@ -1,12 +1,17 @@
 
 
+
 <?php 
-/*
+//laptops controller
+//require_once $_SERVER['DOCUMENT_ROOT'] . '/WDD-341/web/week4/laptop-model.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/WDD-341/web/week4/dbConnect.php';
+
+$db = get_db();
+
 $query = 'SELECT * FROM laptops';
 $stmt = $db($query);
 $stmt->execute();
 $laptops = $stmt->fetchAll(PDO::FETCH_ASSOC);
-*/
 ?>
 
 <main>
