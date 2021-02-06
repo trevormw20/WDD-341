8b0bd232f5e51dc4b7c1ff6ad6efd790113741c4
trevormw20 <trevormw20@gmail.com>
2021-02-06 08:27:22 -0800

    week 4 fix

--- web/week4/displayLaptops.php
+++ web/week4/displayLaptops.php
@@ -11,46 +11,110 @@
 		<h1 class="redBanner">Week 4 Prove Assignment</h1>
 		<main>
 
-			<h3>Select Filters</h1><hr>
-			<form action="index.php" method="post" class="filter-form">
-				<pre>Maker</pre>
-				<input type="radio"  name="maker" value="ASUS">
-				<label for="Acer"> Asus</label><br>
+			<h3>Select Filters</h1>
+				<hr>
+				<form action="index.php" method="post" class="filterForm" id="filterForm">
+					<pre>Maker</pre>
+					<input type="radio" name="maker" value="ASUS">
+					<label for="Acer"> Asus</label><br>
 
-				<input type="radio"  name="maker" value="MSI">
-				<label for="MSI"> MSI</label><br>
+					<input type="radio" name="maker" value="MSI">
+					<label for="MSI"> MSI</label><br>
 
-				<input type="radio"  name="maker" value="ACER">
-				<label for="Acer"> Acer</label><br><br>
-
-				<pre>Ram</pre>
-				<input type="radio"  name="ram" value="8">
-				<label for="8"> 8gb</label><br><br>
-
-				<input type="radio"  name="ram" value="16">
-				<label for="16"> 16gb</label><br>
-
-				<input type="radio"  name="ram" value="32">
-				<label for="32"> 32gb</label><br>
-
-				
-
-				<input type="hidden" name="action" value="filter">
-				<input type="submit" value="Submit">
-			</form>
-			<?php
-
-			echo $laptopDisplay;
+					<input type="radio" name="maker" value="ACER">
+					<label for="Acer"> Acer</label><br><br>
 
 
-			//echo $laptops[0]['laptopmodel'];
+					<pre>Ram</pre>
+					<input type="radio" name="ram" value="8">
+					<label for="8"> 8gb</label><br>
 
-			echo '<pre>';
-			print_r($laptops);
-			echo '</pre>';
+					<input type="radio" name="ram" value="16">
+					<label for="16"> 16gb</label><br>
+
+					<input type="radio" name="ram" value="32">
+					<label for="32"> 32gb</label><br><br>
 
 
-			?>
+					<pre>Cpu</pre>
+					<input type="radio" name="cpu" value="Intel5">
+					<label for="Intel5"> Intel i5</label><br>
+
+					<input type="radio" name="cpu" value="Intel7">
+					<label for="Intel7"> Intel i7</label><br>
+
+					<input type="radio" name="cpu" value="Intel9">
+					<label for="Intel9"> Intel i9</label><br>
+
+					<input type="radio" name="cpu" value="Ryzen5">
+					<label for="Ryzen5"> Ryzen 5</label><br>
+
+					<input type="radio" name="cpu" value="Ryzen7">
+					<label for="Ryzen7"> Ryzen 7</label><br>
+
+					<input type="radio" name="cpu" value="Ryzen9">
+					<label for="Ryzen9"> Ryzen 9</label><br><br>
+
+
+					<pre>Gpu</pre>
+					<input type="radio" name="gpu" value="3060">
+					<label for="3060"> RTX 3060</label><br>
+
+					<input type="radio" name="gpu" value="3070">
+					<label for="3070"> RTX 3070</label><br>
+
+					<input type="radio" name="gpu" value="3080">
+					<label for="3080"> RTX 3080</label><br><br>
+
+
+					<pre>Storage</pre>
+					<input type="radio" name="storage" value="516">
+					<label for="516"> 516 GB</label><br>
+
+					<input type="radio" name="storage" value="1000">
+					<label for="10000"> 1 TB</label><br>
+
+					<input type="radio" name="storage" value="2000">
+					<label for="2000"> 2 TB</label><br><br>
+
+
+					<pre>Screen size</pre>
+					<input type="radio" name="screen" value="13">
+					<label for="13"> 13"</label><br>
+
+					<input type="radio" name="screen" value="14">
+					<label for="14"> 14"</label><br>
+
+					<input type="radio" name="screen" value="15">
+					<label for="15"> 15"</label><br>
+
+					<input type="radio" name="screen" value="17">
+					<label for="17"> 17"</label><br><br>
+
+
+					<input type="hidden" name="action" value="filter">
+					<input type="submit" value="Submit"><br>
+					<input type="button" onclick="resetForm()" value="Reset filters">
+				</form>
+
+				<script>
+					function resetForm() {
+						document.getElementById("filterForm").reset();
+					}
+				</script>
+				<?php
+
+				echo $laptopDisplay;
+
+
+				//echo $laptops[0]['laptopmodel'];
+
+				echo '<pre>';
+				print_r($laptops);
+				echo '</pre>';
+
+
+				?>
 
 		</main>
 	</div>
